788c82035f511f7201399764d7aede8d9c9d09a1
DanielRBowen <danielrbowen@live.com>
2020-11-12 16:51:12 -0700

    Register the CodePagesEncodingProvider.

--- src/DbfDataReader/DbfDataReaderOptions.cs
+++ src/DbfDataReader/DbfDataReaderOptions.cs
@@ -4,10 +4,14 @@ namespace DbfDataReader
 {
     public class DbfDataReaderOptions
     {
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/dotnet/api/system.text.codepagesencodingprovider?redirectedfrom=MSDN&view=net-5.0#Anchor_4
+        /// </summary>
         public DbfDataReaderOptions()
         {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             SkipDeletedRecords = false;
-            Encoding = EncodingProvider.GetEncoding(1252);
+            Encoding = Encoding.GetEncoding(1252); // https://stackoverflow.com/questions/50858209/system-notsupportedexception-no-data-is-available-for-encoding-1252
         }
 
         public bool SkipDeletedRecords { get; set; }
